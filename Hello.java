abd7ab19f5676249b2e3afd0dee5adb4bd1d03a4
jiegegege <741993444@qq.com>
2019-02-14 15:35:18 +0800

    first commit

---
+++ Hello.java
@@ -0,0 +1,25 @@
+package com.sunny.rpc;
+
+import sun.management.Agent;
+
+import java.io.*;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * Hello
+ * 
+ * @author chenzhaoju 
+ */
+public class MyRPC {
+
+    public static void main(String [] a){
+
+        System.out.println("Hello World");
+        
+    }
+
+}
